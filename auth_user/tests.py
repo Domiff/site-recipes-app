e05e86c47a53151d3c334691967cdbfbd1a605fe
dmitriy levykin <dmlegasy@gmail.com>
2026-05-30 21:45:51 +0300

    test: add tests for app

--- auth_user/tests.py
+++ auth_user/tests.py
@@ -1,3 +1,62 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase
+from django.urls import reverse
+from faker import Faker
 
-# Create your tests here.
+User = get_user_model()
+faker = Faker()
+
+
+class RegisterUserTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.password = faker.password()
+        cls.credentials = {
+            "username": faker.user_name(),
+            "email": faker.email(),
+            "password1": cls.password,
+            "password2": cls.password,
+        }
+
+    def test_register(self):
+        response = self.client.post(
+            path=reverse("auth_user:register"), data=self.credentials
+        )
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(
+            User.objects.count(),
+            1,
+        )
+
+
+class LoginUserTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.credentials = {
+            "username": faker.user_name(),
+            "password": faker.password(),
+        }
+        cls.user = User.objects.create_user(
+            username=cls.credentials["username"],
+            password=cls.credentials["password"],
+        )
+
+    def test_login(self):
+        response = self.client.post(
+            path=reverse("auth_user:login"), data=self.credentials
+        )
+        self.assertEqual(response.status_code, 302)
+
+
+class LogoutUserTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.user = User.objects.create_user(
+            username=faker.user_name(),
+            password=faker.password(),
+            email=faker.email(),
+        )
+
+    def test_logout(self):
+        response = self.client.post(path=reverse("auth_user:logout"))
+        self.assertEqual(response.status_code, 302)
--- site_recipes/tests.py
+++ site_recipes/tests.py
@@ -1,3 +1,135 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase
+from django.urls import reverse
+from faker import Faker
 
-# Create your tests here.
+from site_recipes.models import Category, Recipe
+
+User = get_user_model()
+faker = Faker()
+
+
+class ListDetailRecipeTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.user = User.objects.create_user(
+            username=faker.user_name(),
+            password=faker.password(),
+            email=faker.email(),
+        )
+        cls.category1 = Category.objects.create(name=faker.text(100))
+        cls.category2 = Category.objects.create(name=faker.text(100))
+
+        cls.recipe = Recipe.objects.create(
+            title=faker.text(100),
+            description=faker.text(),
+            steps=faker.text(),
+            time_cooking=faker.text(),
+            ingredients=faker.text(),
+            image=faker.url(),
+            author=cls.user,
+        )
+        cls.recipe.category.add(
+            cls.category1,
+            cls.category2,
+        )
+
+    def test_list(self):
+        self.client.force_login(self.user)
+        response = self.client.get(reverse("site_recipes:recipes"))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, self.recipe.title)
+
+    def test_detail(self):
+        self.client.force_login(self.user)
+        response = self.client.get(
+            reverse("site_recipes:detail", args=[self.recipe.id])
+        )
+        self.assertEqual(response.status_code, 200)
+
+
+class CreateUpdateRecipeTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.user = User.objects.create_user(
+            username=faker.user_name(),
+            password=faker.password(),
+            email=faker.email(),
+        )
+        cls.category = Category.objects.create(name=faker.text(100))
+        cls.create_data = {
+            "title": faker.text(100),
+            "description": faker.text(),
+            "steps": faker.text(),
+            "time_cooking": faker.text(),
+            "ingredients": faker.text(),
+            "category": cls.category.pk,
+        }
+        cls.update_data = {
+            "title": faker.text(100),
+            "description": faker.text(),
+            "steps": faker.text(),
+            "time_cooking": faker.text(),
+            "ingredients": faker.text(),
+            "category": cls.category.pk,
+        }
+
+    def test_create(self):
+        self.client.force_login(self.user)
+        response = self.client.post(
+            reverse("site_recipes:create"),
+            data=self.create_data,
+        )
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(
+            Recipe.objects.count(),
+            1,
+        )
+
+    def test_update(self):
+        self.client.force_login(self.user)
+        recipe = Recipe.objects.create(
+            title=faker.text(100),
+            description=faker.text(),
+            steps=faker.text(),
+            time_cooking=faker.text(),
+            ingredients=faker.text(),
+            image=faker.url(),
+            author=self.user,
+        )
+        recipe.category.add(self.category)
+        response = self.client.post(
+            reverse("site_recipes:update", args=[recipe.pk]),
+            data=self.update_data,
+        )
+        self.assertEqual(response.status_code, 302)
+
+
+class DeleteRecipeTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.user = User.objects.create_user(
+            username=faker.user_name(),
+            password=faker.password(),
+            email=faker.email(),
+        )
+        cls.recipe = Recipe.objects.create(
+            title=faker.text(100),
+            description=faker.text(),
+            steps=faker.text(),
+            time_cooking=faker.text(),
+            ingredients=faker.text(),
+            image=faker.url(),
+            author=cls.user,
+        )
+
+    def test_delete(self):
+        self.client.force_login(self.user)
+        response = self.client.delete(
+            reverse("site_recipes:delete", args=[self.recipe.pk])
+        )
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(
+            Recipe.objects.count(),
+            0,
+        )
