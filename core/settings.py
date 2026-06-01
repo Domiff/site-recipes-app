4ebb1035dd239b31e1797075b1d81d3deb2dd99f
dmitriy levykin <dmlegasy@gmail.com>
2026-06-01 21:55:52 +0300

    refactor: update CSRF_TRUSTED_ORIGINS

--- core/settings.py
+++ core/settings.py
@@ -147,4 +147,6 @@ MEDIA_ROOT = BASE_DIR / "media/"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-CSRF_TRUSTED_ORIGINS = [os.getenv("CSRF_TRUSTED_ORIGIN")]
+CSRF_TRUSTED_ORIGINS = [
+    os.environ.get("CSRF_TRUSTED_ORIGIN", ["http://127.0.0.1:8080"])
+]
