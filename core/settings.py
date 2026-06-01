0ace3ede564e756fb9ce6d121b9f619d89a0e0ec
dmitriy levykin <dmlegasy@gmail.com>
2026-06-01 21:14:52 +0300

    fix(auth): add CSRF_TRUSTED_ORIGIN

--- core/settings.py
+++ core/settings.py
@@ -147,3 +147,4 @@ MEDIA_ROOT = BASE_DIR / "media/"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+CSRF_TRUSTED_ORIGINS = [os.getenv("CSRF_TRUSTED_ORIGIN")]
